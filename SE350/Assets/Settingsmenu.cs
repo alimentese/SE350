af9cd4199944eff951f9d8ca90dfb6f8fb5aa392
BurakY2 <yalcnburakk@gmail.com>
2020-04-09 00:53:50 +0300

    9.04.2020 SON VERSİON

--- SE350/Assets/Settingsmenu.cs
+++ SE350/Assets/Settingsmenu.cs
@@ -2,9 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settingsmenu : MonoBehaviour
 {
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for(int i=0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
   public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
