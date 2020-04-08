2713329735011736d66565211f86aa8fe16715f1
alimentese <ali_mentese@hotmail.com>
2020-04-09 00:26:02 +0300

    ilk sürüm

---
+++ new project/Assets/Mainmenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+   
+    public void BacktoMainScreen(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void BacktoClassSelect(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ new project/Assets/Settingsmenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settingsmenu : MonoBehaviour
+{
+  public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreeen)
+    {
+        Screen.fullScreen = isFullscreeen;
+    }
+}
